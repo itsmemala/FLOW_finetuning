b14ff876d57b62e6faa03cdebe71fbd450ee0570
Malavika Suresh <itsmemala@users.noreply.github.com>
2026-01-22 14:54:37 +0530

    Update misc.py

--- language/utils/misc.py
+++ language/utils/misc.py
@@ -68,7 +68,8 @@ def compute_mas_wgts(model, train, args):
     model.train()
     for batch in tqdm(train):
         batch = {k: v.to(args.device) for k, v in batch.items()}
-        outputs = model(**batch)
+        output = model(**batch)
+        print(output.shape)
         break
     
     model.zero_grad()
